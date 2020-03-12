fa56de0a40156f1b8b5751ffe6170bcf6ba55738
aierong <aierong@126.com>
2020-03-12 14:14:22 +0800

    company

--- service/common/common.js
+++ service/common/common.js
@@ -7,6 +7,8 @@
 
 var dayjs = require( 'dayjs' )
 const UUID = require( 'uuid' )
+//引入
+var CryptoJS = require( "crypto-js" );
 
 module.exports = {
 
@@ -22,10 +24,24 @@ module.exports = {
 
         return now.format( formatstring );
     } ,
+    /**
+     * 得guid字符串
+     * @returns {*}
+     * @constructor
+     */
     GetGuid : () => {
         var u1 = UUID.v1();  //基于时间戳生成  （time-based）
 
         return u1;
+    } ,
+    /**
+     * 加密字符串
+     * @param str
+     * @param key
+     * @constructor
+     */
+    EncryptString : ( str = '' , key = 'key' ) => {
+
     } ,
 }
 

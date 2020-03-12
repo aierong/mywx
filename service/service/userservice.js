95abbe0ccb8a3aaf203498d808963fa70a9715b3
aierong <aierong@126.com>
2020-03-12 11:13:45 +0800

    company

--- service/service/userservice.js
+++ service/service/userservice.js
@@ -1,11 +1,33 @@
 /*
-作者: chenghao
-Date: 2020/3/12
-Time: 10:41
-功能: js脚本
-*/
+ 作者: chenghao
+ Date: 2020/3/12
+ Time: 10:41
+ 功能: 用户的相关服务
+ */
 
+var mongoose = require( 'mongoose' );
+const user = require( './modules/user.js' )
 
+module.exports = {
+    /**
+     * 判断手机号码是否存在
+     * @param mobile
+     * @returns {Promise<*>}
+     * @constructor
+     */
+    IsExistsMobile : async ( mobile = '' ) => {
 
+        //构建条件
+        let where = {
+            mobile : mobile
+        }
+
+        let isexists = await user.exists( where );
+
+        // console.log( 'newkit ' , newkit )
+
+        return isexists;
+    } ,
+}
 
 

c572036485d2ca93a7cffc389369d341aada0640
aierong <aierong@126.com>
2020-03-12 11:02:32 +0800

    company

---
+++ service/service/modules/user.js
@@ -0,0 +1,39 @@
+/*
+ 作者: chenghao
+ Date: 2020/3/12
+ Time: 10:42
+ 功能: js脚本
+ */
+
+var mongoose = require( 'mongoose' );
+
+//定义Schema
+var kittySchema = mongoose.Schema( {
+    ids : String ,
+    mobile : String ,
+    avatar : String ,
+    password : String ,
+
+    name : String ,
+    email : String ,
+
+    //最后一次登录时间
+    lastlogindate : String ,
+    //累计登录次数
+    logintimes : {
+
+        type : Number ,
+        min : 0
+
+    } ,
+
+    adddate : String ,
+    updatedate : String ,
+    deletedate : String ,
+
+} , {
+    //versionKey =false 禁用版本，要不每次插入数据都带一个__v
+    versionKey : false
+} );
+
+

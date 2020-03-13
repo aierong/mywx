42dde6fca79010ce511482b6c99ffbddb40a1d77
aierong <aierong@126.com>
2020-03-13 23:22:36 +0800

    home

--- service/service/userservice.js
+++ service/service/userservice.js
@@ -9,89 +9,108 @@ var mongoose = require( 'mongoose' );
 const usermodel = require( './modules/user.js' )
 const common = require( '../common/common.js' )
 
-module.exports = {
-    /**
-     * 判断手机号码是否存在
-     * @param mobile
-     * @returns {Promise<*>}
-     * @constructor
-     */
-    IsExistsMobile : async ( mobile = '' ) => {
+/**
+ * 判断手机号码是否存在
+ * @param mobile
+ * @returns {Promise<*>}
+ * @constructor
+ */
+async function IsExistsMobile ( mobile = '' ) {
 
-        //构建条件
-        let where = {
-            mobile : mobile
-        }
+    //构建条件
+    let where = {
+        mobile : mobile
+    }
 
-        let isexists = await usermodel.exists( where );
+    let isexists = await usermodel.exists( where );
 
-        // console.log( 'newkit ' , newkit )
+    return isexists;
+}
 
-        return isexists;
-    } ,
-    /**
-     * 添加用户
-     * @param postData
-     * @returns {Promise<any>}
-     * @constructor
-     */
-    AddUser : async ( postData ) => {
-        //数据解构出来
-        let { mobile , avatar , password , name , email } = postData;
+/**
+ * 添加用户
+ * @param postData
+ * @returns {Promise<any>}
+ * @constructor
+ */
+async function AddUser ( postData ) {
+    //数据解构出来
+    let { mobile , avatar , password , name , email } = postData;
 
-        // console.log( '收到password' , password )
+    // console.log( '收到password' , password )
 
-        //密码要再次加密一次
-        password = common.EncryptPassWord( password );
+    //密码要再次加密一次
+    password = common.EncryptPassWord( password );
 
-        //构建数据
-        var newuser = new usermodel( {
+    //构建数据
+    var newuser = new usermodel( {
 
-            mobile ,
-            avatar ,
-            password ,
-            name ,
-            email ,
-            lastlogindate : '' ,
-            logintimes : 0 ,
+        mobile ,
+        avatar ,
+        password ,
+        name ,
+        email ,
+        lastlogindate : '' ,
+        logintimes : 0 ,
 
-            // 最新时间
-            adddate : common.GetNowString() ,
-            // 搞一个guid
-            ids : common.GetGuid()
-        } );
+        // 最新时间
+        adddate : common.GetNowString() ,
+        // 搞一个guid
+        ids : common.GetGuid()
+    } );
 
-        let newobj = await newuser.save();
+    let newobj = await newuser.save();
 
-        // console.log( 'newkit ' , newkit )
+    // console.log( 'newkit ' , newkit )
 
-        return true;
-    } ,
-    GetUserByMobile : async ( mobile = '' ) => {
-        let where = {
-            mobile : mobile
-        };
+    return true;
+}
 
-        let obj = await usermodel.findOne( where );
+/**
+ *
+ * @param mobile
+ * @returns {Promise<*>}
+ * @constructor
+ */
+async function GetUserByMobile ( mobile = '' ) {
+    let where = {
+        mobile : mobile
+    };
 
-        return obj;
-    } ,
-    RunLogin : async ( mobile = '' , logintimes = 0 ) => {
-        let where = {
-            mobile : mobile
-        };
+    let obj = await usermodel.findOne( where );
 
-        let obj = await usermodel.findOneAndUpdate( where , [
-            {
-                $set : {
-                    logintimes : { $add : [ { $ifNull : [ "$logintimes" , 0 ] } , 1 ] } ,
-                    lastlogindate : common.GetNowString()
-                }
+    return obj;
+}
+
+/**
+ *
+ * @param mobile
+ * @param logintimes
+ * @returns {Promise<*>}
+ * @constructor
+ */
+async function RunLogin ( mobile = '' , logintimes = 0 ) {
+    let where = {
+        mobile : mobile
+    };
+
+    let obj = await usermodel.findOneAndUpdate( where , [
+        {
+            $set : {
+                logintimes : { $add : [ { $ifNull : [ "$logintimes" , 0 ] } , 1 ] } ,
+                lastlogindate : common.GetNowString()
             }
-        ] );
+        }
+    ] );
 
-        return obj;
-    } ,
+    return obj;
+}
+
+module.exports = {
+    IsExistsMobile ,
+    AddUser ,
+    GetUserByMobile ,
+    RunLogin
 }
 
 

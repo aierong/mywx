5d6134c259e2173407f0a31d21abd6a386a3868a
aierong <aierong@126.com>
2020-03-14 11:10:51 +0800

    home

--- service/service/userphotoservice.js
+++ service/service/userphotoservice.js
@@ -1,11 +1,13 @@
 /*
-作者: chenghao
-Date: 2020/3/14
-Time: 11:09
-功能: js脚本
-*/
+ 作者: chenghao
+ Date: 2020/3/14
+ Time: 11:09
+ 功能: js脚本
+ */
 
-
-
+var mongoose = require( 'mongoose' );
+const userphotomodel = require( './modules/userphoto.js' )
+const common = require( '../common/common.js' )
+const log = require( '../common/log' )
 
 

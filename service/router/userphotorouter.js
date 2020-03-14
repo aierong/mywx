b4e297aa3737ad975249fbdc3d80370cfa3501d1
aierong <aierong@126.com>
2020-03-14 17:54:49 +0800

    home

--- service/router/userphotorouter.js
+++ service/router/userphotorouter.js
@@ -5,7 +5,18 @@
  功能: js脚本
  */
 
+// require( 'koa-router' )()是引入并且初始化 ,等同于下面2句
+// const Router = require('koa-router')
+// const router = new Router()
+const router = require( 'koa-router' )()
 
+const userphotocontroller = require( '../controller/userphoto' )
 
+let prefix = '/api/userphoto';
+
+router.get( `${ prefix }/getuserphoto/:mobile` , userphotocontroller.GetUserPhoto )
+router.post( `${ prefix }/save` , userphotocontroller.Save )
+
+module.exports = router
 
 

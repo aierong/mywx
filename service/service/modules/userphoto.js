e5db3cafa3b95df21e286450fb973d547eaa48c7
aierong <aierong@126.com>
2020-03-14 11:09:17 +0800

    home

--- service/service/modules/userphoto.js
+++ service/service/modules/userphoto.js
@@ -5,7 +5,44 @@
  功能: js脚本
  */
 
+var mongoose = require( 'mongoose' );
 
+//定义Schema
+var UserPhotoSchema = mongoose.Schema( {
+    ids : {
+        type : String ,
+        unique : true
+    } ,
+    mobile : {
+        type : String ,
+        unique : true
+    } ,
+    isshare : {
+        type : Boolean ,
+        default : false
+    } ,
+    imgs : {
+        type : Array
+    } ,
+    adddate : {
+        type : String ,
+        default : ''
+    } ,
+    updatedate : {
+        type : String ,
+        default : ''
+    } ,
+    deletedate : {
+        type : String ,
+        default : ''
+    } ,
 
+} , {
+    //versionKey =false 禁用版本，要不每次插入数据都带一个__v
+    versionKey : false
+} );
+
+let userphotomodel = mongoose.model( 'userphoto' , UserPhotoSchema , 'userphoto' );
+module.exports = userphotomodel;
 
 

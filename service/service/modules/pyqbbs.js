18adb044ef67a2ea335587b6046e8ceb42ceac27
aierong <aierong@126.com>
2020-03-15 14:08:37 +0800

    home

--- service/service/modules/pyqbbs.js
+++ service/service/modules/pyqbbs.js
@@ -7,6 +7,56 @@
 
 var mongoose = require( 'mongoose' );
 
+//定义Schema
+var PyqBbsSchema = mongoose.Schema( {
+    ids : {
+        type : String ,
+        unique : true
+    } ,
+    date : {
+        type : String ,
+        default : ''
+    } ,
+
+    mobile : {
+        type : String ,
+        default : ''
+    } ,
+
+    name : {
+        type : String ,
+        default : ''
+    } ,
+    avatar : {
+        type : String ,
+        default : ''
+    } ,
+
+    txt : {
+        type : String ,
+        default : ''
+    } ,
+
+    adddate : {
+        type : String ,
+        default : ''
+    } ,
+    updatedate : {
+        type : String ,
+        default : ''
+    } ,
+    deletedate : {
+        type : String ,
+        default : ''
+    } ,
+
+} , {
+    //versionKey =false 禁用版本，要不每次插入数据都带一个__v
+    versionKey : false
+} );
+
+let pyqbbsmodel = mongoose.model( 'pyqbbs' , PyqBbsSchema , 'pyqbbs' );
+module.exports = pyqbbsmodel;
 
 
 

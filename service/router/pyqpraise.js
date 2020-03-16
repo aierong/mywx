0eec503429e36af0abe333fd40024df519dfb2b4
aierong <aierong@126.com>
2020-03-16 18:21:10 +0800

    company

--- service/router/pyqpraise.js
+++ service/router/pyqpraise.js
@@ -5,7 +5,15 @@
  功能: js脚本
  */
 
+// require( 'koa-router' )()是引入并且初始化 ,等同于下面2句
+// const Router = require('koa-router')
+// const router = new Router()
+const router = require( 'koa-router' )()
 
+const pyqpraisecontroller = require( '../controller/pyqpraise' )
 
+let prefix = '/api/pyqpraise';
 
+router.post( `${ prefix }/add` , pyqpraisecontroller.Add )
 
+module.exports = router

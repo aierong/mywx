7069eaf87b08856dde370e1e18af002c452ba51c
aierong <aierong@126.com>
2020-03-16 18:20:00 +0800

    company

--- service/controller/pyqpraise.js
+++ service/controller/pyqpraise.js
@@ -9,6 +9,58 @@
 const pyqpraiseservice = require( '../service/pyqpraiseservice' )
 const common = require( '../common/common.js' )
 
+module.exports = {
+    Add : async ( ctx , next ) => {
+        //  http://localhost:3001/api/pyqpraise/add
+
+        let result = {
+
+            isok : true ,
+            errmsg : '' ,
+
+        }
+
+        //先接收post的参数
+        //接收到post数据 postData是一个对象
+        let postData = ctx.request.body;
+
+        let { mobile , pyq_id } = postData;
+        let ischeck = await pyqpraiseservice.PraiseCheck( true , mobile , pyq_id );
+
+        if ( !ischeck ) {
+            //检查不合格
+            result = {
+
+                isok : false ,
+                errmsg : '不允许点赞' ,
+
+            }
+
+        }
+        else {
+            let obj = await pyqpraiseservice.Add( postData );
+
+            if ( obj != null ) {
+                result = {
+
+                    isok : true ,
+                    errmsg : '' ,
+
+                }
+            }
+            else {
+                result = {
+
+                    isok : false ,
+                    errmsg : '点赞失败' ,
+
+                }
+            }
+        }
+
+        ctx.body = result;
+    } ,
+}
 
 
 

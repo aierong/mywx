209e019cb315b5a60f8744acf52177f3fb6138c3
aierong <aierong@126.com>
2020-03-17 17:17:35 +0800

    company

--- src/common/util/util.js
+++ src/common/util/util.js
@@ -4,6 +4,8 @@
  定义一些公用方法
  */
 
+import CryptoJS from 'crypto-js'
+
 /**
  *
  * @param val
@@ -20,4 +22,30 @@ function GetIconAllName ( val ) {
 
 }
 
-export { GetIconAllName }
+/**
+ * 加密字符串
+ * @param str
+ * @returns {*}
+ * @constructor
+ */
+function EncryptString ( str = '' ) {
+
+    var ciphertext = CryptoJS.MD5( str ).toString()
+
+    return ciphertext;
+}
+
+/**
+ * 加密密码
+ * @param str
+ * @returns {*}
+ * @constructor
+ */
+function EncryptPassWord ( pwd = '' ) {
+    return EncryptString( pwd );
+}
+
+export {
+    GetIconAllName ,
+    EncryptPassWord
+}

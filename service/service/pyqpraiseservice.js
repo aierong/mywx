ac3c970396606e4a6c1bd74d8f16563c386f009b
aierong <aierong@126.com>
2020-03-17 17:47:11 +0800

    company

--- service/service/pyqpraiseservice.js
+++ service/service/pyqpraiseservice.js
@@ -46,6 +46,48 @@ async function Add ( postData ) {
         newmodel.save() ,
         pyqservice.UpdatePraiseCount( pyq_id , 1 ) ,
         log.AddRunLog( mobile , 'AddPyqPraise' , `用户${ mobile }(${ name })点赞一个朋友圈 朋友圈_id:${ pyq_id }` )
+
+    ] );
+
+    let newobj = null;
+
+    if ( result != null && result.length >= 3 ) {
+        newobj = result[ 1 ];  //这里返回 朋友圈 这条记录
+    }
+
+    return newobj;
+}
+
+/**
+ * 取消点赞
+ * @param postData
+ * @returns {Promise<null>}
+ * @constructor
+ */
+async function Delete ( postData ) {
+    //数据解构出来
+    let { pyq_id , name , mobile } = postData;
+
+    let nowstr = common.GetNowString();
+
+    let where = {
+        pyq_id : pyq_id ,
+        mobile : mobile ,
+        iscancel : false
+    }
+
+    //做3件事情:1.记录点赞流水 2.更新朋友圈表点赞数量 3.记录日志
+    var result = await Promise.all( [
+
+        pyqpraisemodel.findOneAndUpdate( where , {
+            deletedate : nowstr ,
+            iscancel : true ,
+        } , {
+            new : true
+        } ) ,
+        pyqservice.UpdatePraiseCount( pyq_id , -1 ) , //数量减少1
+        log.AddRunLog( mobile , 'DeletePyqPraise' , `用户${ mobile }(${ name })取消点赞一个朋友圈 朋友圈_id:${ pyq_id }` )
+
     ] );
 
     let newobj = null;
@@ -99,6 +141,7 @@ async function PraiseCheck ( ispraise , mobile , pyq_id ) {
 module.exports = {
 
     Add ,
+    Delete ,
     PraiseCheck
 
 }

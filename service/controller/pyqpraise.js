6ec6470cdc8ff0995eb6a426741ca38398687b60
aierong <aierong@126.com>
2020-03-19 19:07:53 +0800

    home

--- service/controller/pyqpraise.js
+++ service/controller/pyqpraise.js
@@ -63,7 +63,52 @@ module.exports = {
     Delete : async ( ctx , next ) => {
         //  http://localhost:3001/api/pyqpraise/delete
 
+        let result = {
 
+            isok : true ,
+            errmsg : '' ,
+
+        }
+
+        //先接收post的参数
+        //接收到post数据 postData是一个对象
+        let postData = ctx.request.body;
+
+        let { mobile , pyq_id } = postData;
+        let ischeck = await pyqpraiseservice.PraiseCheck( false , mobile , pyq_id );
+
+        if ( !ischeck ) {
+            //检查不合格
+            result = {
+
+                isok : false ,
+                errmsg : '不允许取消点赞' ,
+
+            }
+
+        }
+        else {
+            let obj = await pyqpraiseservice.Delete( postData );
+
+            if ( obj != null ) {
+                result = {
+
+                    isok : true ,
+                    errmsg : '' ,
+
+                }
+            }
+            else {
+                result = {
+
+                    isok : false ,
+                    errmsg : '取消点赞失败' ,
+
+                }
+            }
+        }
+
+        ctx.body = result;
     } ,
 }
 

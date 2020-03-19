68010c2305daaf1c328156b533d09d881e49d3fd
aierong <aierong@126.com>
2020-03-19 19:00:52 +0800

    home

--- service/service/pyqbbsservice.js
+++ service/service/pyqbbsservice.js
@@ -61,8 +61,49 @@ async function Add ( postData ) {
     return newobj;
 }
 
+/**
+ * 删除评论
+ * @param postData
+ * @returns {Promise<null>}
+ * @constructor
+ */
+async function Delete ( postData ) {
+    //数据解构出来
+    let { pyq_id , name , mobile , _id } = postData;
+
+    let now = new Date();
+    let nowstr = common.GetNowString( now );
+
+    let where = {
+        _id : _id
+    }
+
+    //做3件事情:1.记录点赞流水 2.更新朋友圈表点赞数量 3.记录日志
+    var result = await Promise.all( [
+
+        pyqbbsmodel.findOneAndUpdate( where , {
+            deletedate : nowstr ,
+            isdelete : true ,
+        } , {
+            new : true
+        } ) ,
+        pyqservice.UpdateBbsCount( pyq_id , -1 ) , //数量减少1
+        log.AddRunLog( mobile , 'DeletePyqBbs' , `用户${ mobile }(${ name })删除一个朋友圈评论 朋友圈_id:${ pyq_id }` )
+
+    ] );
+
+    let newobj = null;
+
+    if ( result != null && result.length >= 3 ) {
+        newobj = result[ 1 ];  //这里返回 朋友圈 这条记录
+    }
+
+    return newobj;
+}
+
 module.exports = {
 
     Add ,
+    Delete
 
 }

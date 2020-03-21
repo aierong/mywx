87848a59a0a71121506072589a55d28f65b1ee67
aierong <aierong@126.com>
2020-03-21 18:23:39 +0800

    company

--- service/controller/pyqpraise.js
+++ service/controller/pyqpraise.js
@@ -11,116 +11,6 @@ const pyqservice = require( '../service/pyqservice' )
 const common = require( '../common/common.js' )
 
 module.exports = {
-    // Add : async ( ctx , next ) => {
-    //     //  http://localhost:3001/api/pyqpraise/add
-    //
-    //     let result = {
-    //
-    //         isok : true ,
-    //         errmsg : '' ,
-    //         data : null
-    //
-    //     }
-    //
-    //     //先接收post的参数
-    //     //接收到post数据 postData是一个对象
-    //     let postData = ctx.request.body;
-    //
-    //     let { mobile , pyq_id } = postData;
-    //     let ischeck = await pyqpraiseservice.PraiseCheck( true , mobile , pyq_id );
-    //
-    //     if ( !ischeck ) {
-    //         //检查不合格
-    //         result = {
-    //
-    //             isok : false ,
-    //             errmsg : '不允许点赞' ,
-    //             data : null
-    //         }
-    //
-    //     }
-    //     else {
-    //         let obj = await pyqpraiseservice.Add( postData );
-    //
-    //         if ( obj ) {
-    //
-    //             let _data = await pyqservice.GetPyqById( pyq_id );  //取最新的这条记录回来
-    //
-    //             result = {
-    //
-    //                 isok : true ,
-    //                 errmsg : '' ,
-    //                 data : _data
-    //             }
-    //         }
-    //         else {
-    //             result = {
-    //
-    //                 isok : false ,
-    //                 errmsg : '点赞失败' ,
-    //                 data : null
-    //
-    //             }
-    //         }
-    //     }
-    //
-    //     ctx.body = result;
-    // } ,
-    // Delete : async ( ctx , next ) => {
-    //     //  http://localhost:3001/api/pyqpraise/delete
-    //
-    //     let result = {
-    //
-    //         isok : true ,
-    //         errmsg : '' ,
-    //         data : null
-    //     }
-    //
-    //     //先接收post的参数
-    //     //接收到post数据 postData是一个对象
-    //     let postData = ctx.request.body;
-    //
-    //     let { mobile , pyq_id } = postData;
-    //     let ischeck = await pyqpraiseservice.PraiseCheck( false , mobile , pyq_id );
-    //
-    //     if ( !ischeck ) {
-    //         //检查不合格
-    //         result = {
-    //
-    //             isok : false ,
-    //             errmsg : '不允许取消点赞' ,
-    //             data : null
-    //
-    //         }
-    //
-    //     }
-    //     else {
-    //         let obj = await pyqpraiseservice.Delete( postData );
-    //
-    //         if ( obj ) {
-    //             let _data = await pyqservice.GetPyqById( pyq_id );  //取最新的这条记录回来
-    //
-    //             result = {
-    //
-    //                 isok : true ,
-    //                 errmsg : '' ,
-    //                 data : _data
-    //
-    //             }
-    //         }
-    //         else {
-    //             result = {
-    //
-    //                 isok : false ,
-    //                 errmsg : '取消点赞失败' ,
-    //                 data : null
-    //
-    //             }
-    //         }
-    //     }
-    //
-    //     ctx.body = result;
-    // } ,
 
     Praise : async ( ctx , next ) => {
         //  http://localhost:3001/api/pyqpraise/praise
@@ -143,15 +33,15 @@ module.exports = {
         let { mobile , pyq_id } = postData;
         let _IsPraiseStatus = await pyqpraiseservice.IsPraiseStatus( mobile , pyq_id );
 
-        // console.log( 'Praise _IsPraiseStatus' , _IsPraiseStatus )
+        console.log( 'Praise _IsPraiseStatus' , _IsPraiseStatus )
 
         let obj = false;
 
-        if ( _IsPraiseStatus ) {
-            obj = await pyqpraiseservice.Add( postData );
+        if ( _IsPraiseStatus.IsPraise ) {
+            obj = await pyqpraiseservice.Add( postData , _IsPraiseStatus.id );
         }
         else {
-            obj = await pyqpraiseservice.Delete( postData );
+            obj = await pyqpraiseservice.Delete( postData , _IsPraiseStatus.id );
         }
 
         // console.log( 'Praise obj' , obj )
--- service/service/pyqpraiseservice.js
+++ service/service/pyqpraiseservice.js
@@ -18,7 +18,7 @@ const pyqservice = require( './pyqservice.js' )
  * @returns {Promise<null>}
  * @constructor
  */
-async function Add ( postData ) {
+async function Add ( postData , id ) {
     //数据解构出来
     let { mobile , avatar , name , pyq_id } = postData;
 
@@ -44,12 +44,29 @@ async function Add ( postData ) {
         ids : common.GetGuid()
     } );
 
+    let updatecount = pyqservice.UpdatePraiseCount( pyq_id , 1 );
+    let addlog = log.AddRunLog( mobile , 'AddPyqPraise' , `用户${ mobile }(${ name })点赞一个朋友圈 朋友圈_id:${ pyq_id }` );
+
+    let savedata = newmodel.save();
+    let updatedata = null;
+    if ( id != '' ) {
+        updatedata = pyqpraisemodel.findOneAndUpdate( {
+            _id : mongoose.Types.ObjectId( id )
+        } , {
+            updatedate : nowstr ,
+            iscancel : false ,
+        } , {
+            new : true
+        } );
+
+    }
+
     //做3件事情:1.记录点赞流水 2.更新朋友圈表点赞数量 3.记录日志
     var result = await Promise.all( [
 
-        newmodel.save() ,
-        pyqservice.UpdatePraiseCount( pyq_id , 1 ) ,
-        log.AddRunLog( mobile , 'AddPyqPraise' , `用户${ mobile }(${ name })点赞一个朋友圈 朋友圈_id:${ pyq_id }` )
+        id == '' ? savedata : updatedata ,
+        updatecount ,
+        addlog
 
     ] );
 
@@ -71,7 +88,7 @@ async function Add ( postData ) {
  * @returns {Promise<null>}
  * @constructor
  */
-async function Delete ( postData ) {
+async function Delete ( postData , id ) {
     //数据解构出来
     let { pyq_id , name , mobile } = postData;
 
@@ -79,9 +96,7 @@ async function Delete ( postData ) {
     let nowstr = common.GetNowString( now );
 
     let where = {
-        pyq_id : pyq_id ,
-        mobile : mobile ,
-        iscancel : false
+        _id : mongoose.Types.ObjectId( id )
     }
 
     //做3件事情:1.记录点赞流水 2.更新朋友圈表点赞数量 3.记录日志
@@ -111,45 +126,6 @@ async function Delete ( postData ) {
 }
 
 /**
- * 点赞检查
- * @param ispraise
- * @param mobile
- * @param pyq_id
- * @returns {Promise<boolean>}
- * @constructor
- */
-// async function PraiseCheck ( ispraise , mobile , pyq_id ) {
-//     let where = {
-//         pyq_id : pyq_id ,
-//         mobile : mobile ,
-//         iscancel : false
-//
-//     }
-//
-//     let isexists = false;
-//
-//     isexists = await pyqpraisemodel.exists( where );
-//
-//     if ( ispraise ) {
-//
-//         // 存在点赞记录，就不可以再点赞
-//         if ( isexists ) {
-//             return false;
-//         }
-//     }
-//     else {
-//         // isexists = await pyqpraisemodel.exists( where );
-//
-//         // 不存在未取消的点赞记录，不允许取消点赞
-//         if ( !isexists ) {
-//             return false;
-//         }
-//     }
-//
-//     return true;
-// }
-
-/**
  * 是可以点赞状态
  * @param mobile
  * @param pyq_id
@@ -157,6 +133,8 @@ async function Delete ( postData ) {
  * @constructor
  */
 async function IsPraiseStatus ( mobile , pyq_id ) {
+
+    //只搞 没有删除的
     let where = {
 
         pyq_id : pyq_id ,
@@ -165,22 +143,42 @@ async function IsPraiseStatus ( mobile , pyq_id ) {
 
     }
 
+    let result = {
+        IsPraise : false ,
+        id : ''
+    };
+
     //把这个用户，这个帖子的 最后一个流水找回来
     let obj = await pyqpraisemodel.findOne( where ).sort( { addunix : -1 } );
 
+    console.log( 'IsPraiseStatus obj' , obj )
+
     if ( obj != null ) {
+
+        //把id记录下来
+        result = {
+            id : obj._id
+        };
+
         if ( obj.iscancel ) {
-            return true;  //可以点赞
+            //可以点赞
+            result.IsPraise = true;
         }
         else {
-            return false;  //可以取消点赞
+            //可以取消点赞
+            result.IsPraise = false;
         }
     }
     else {
         //记录 为空，说明还没有搞过
-        return true;  //可以点赞
+        //可以点赞
+        result = {
+            IsPraise : true ,
+            id : ''
+        };
     }
 
+    return result;
 }
 
 module.exports = {
@@ -188,6 +186,5 @@ module.exports = {
     Add ,
     Delete ,
     IsPraiseStatus ,
-    // PraiseCheck
 
 }

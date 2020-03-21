9c82b60ee3b08ad3c1aab88d2fe192c9b07abe31
aierong <aierong@126.com>
2020-03-21 11:49:04 +0800

    company

--- src/https/api/pyqpraise.js
+++ src/https/api/pyqpraise.js
@@ -5,7 +5,28 @@
  功能: js脚本
  */
 
+import axios from 'axios'
 
+let prefix = '/pyqpraise';
 
+/**
+ * 点赞
+ * @param mobile
+ * @param avatar
+ * @param name
+ * @param pyq_id
+ * @returns {Promise<AxiosResponse<T>>}
+ */
+export const addpraise = ( mobile , avatar , name , pyq_id ) => {
+    let obj = {
+        mobile ,
+        avatar ,
+        name ,
+        pyq_id ,
+
+    }
+
+    return axios.post( `${ prefix }/add` , obj );
+}
 
 

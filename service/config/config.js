1f4fdbd9ded96ace0d46222398a80bc35f7fbd56
aierong <aierong@126.com>
2020-04-11 15:38:32 +0800

    home

---
+++ service/config/config.js
@@ -0,0 +1,17 @@
+/*
+ 作者: chenghao
+ Date: 2020/3/29
+ Time: 12:59
+ 功能: js脚本
+ */
+
+module.exports = {
+    //定义定义密钥
+    //My_Token_Key
+    TokenPrivateKey : "c2f26f24-1ea0-4389-9536-8a64ab0d5642" ,
+
+    ServerPort : 3001
+}
+
+
+
